a98453070062f29a82a447595367b7e333a1f883
遠山聡美 <S083@INV539noMacBook-Pro.local>
2018-03-21 18:57:42 +0900

    スライダー部分の作成

--- 5_RGBApp/ViewController.swift
+++ 5_RGBApp/ViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Rのラベル
+    @IBOutlet weak var rLabel: UILabel!
+    //Gのラベル
+    @IBOutlet weak var gLabel: UILabel!
+    //Bのラベル
+    @IBOutlet weak var bLabel: UILabel!
+    
+    
+    //Rスライダーの動作
+    @IBAction func rSliderAction(_ sender: UISlider) {
+        rLabel.text = "R:\(Int(sender.value))"
+    }
+    
+    //Gスライダーの動作
+    @IBAction func gSliderAction(_ sender: UISlider) {
+        gLabel.text = "G:\(Int(sender.value))"
+    }
+
+    //Bスライダーの動作
+    @IBAction func bSliderAction(_ sender: UISlider) {
+        bLabel.text = "B:\(Int(sender.value))"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
 }
 
